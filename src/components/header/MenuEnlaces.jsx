ea27851c378e08d81259b4e5c5138814bf0a885f
Busferis <busferis7@gmail.com>
2023-08-02 20:56:21 -0300

    commit

---
+++ src/components/header/MenuEnlaces.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const MenuEnlaces = () => {
+    return (
+        <>
+            <ul class="toro">
+                <li>CATEGORÍAS</li>
+                <li>SALONES</li>
+                <li>IMAGEN PERSONAL</li>
+                <li>CONTACTO</li>
+            </ul>
+        </>
+    )
+    
+}
+
+export default MenuEnlaces
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,8 +1,26 @@
-import React from "react";// No importa, si se usa Vite, ya que lo hace por inercia.
+import React from "react" // No importa, si se usa Vite, ya que lo hace por inercia.
+import MenuEnlaces from "./MenuEnlaces"
+import "./header.css"
+import "../../assets/logoportal.png"
 
 const Encabezado = () => {// No nombrar funciones con etiquetas reservadas de HTML como header, body, etc.
     return (
-        <div>header</div>
+        <>
+            <header class="header">
+                <div class="menu">
+                    <img src="../../assets/logoportal.png"></img>
+                    <nav>
+                        <MenuEnlaces />
+                    </nav>
+                    <div class="empresas">
+                        <span class="material-icons vaca">
+                            login
+                        </span>
+                        <h6 class="vaca">EMPRESAS</h6>
+                    </div>
+                </div>
+            </header>
+        </>
     )
 }
 
