91c436a503d3bba78a94a4879475a01b48ee23dc
Busferis <busferis7@gmail.com>
2023-08-02 20:21:40 -0300

    commit

---
+++ src/components/header/header.jsx
@@ -0,0 +1,9 @@
+import React from "react";// No importa, si se usa Vite, ya que lo hace por inercia.
+
+const Encabezado = () => {// No nombrar funciones con etiquetas reservadas de HTML como header, body, etc.
+    return (
+        <div>header</div>
+    )
+}
+
+export default Encabezado
