9155d4384a8b179362a8900067287c611d133105
Busferis <busferis7@gmail.com>
2023-08-09 21:09:27 -0300

    Actualizacion

--- src/components/header/Login.jsx
+++ src/components/header/Login.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='flex'>
+        <i className='material-icons-outlined'></i>
+        <h6>Nombre</h6>
+    </div>
   )
 }
 
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,22 +2,20 @@ import React from "react" // No importa, si se usa Vite, ya que lo hace por iner
 import MenuEnlaces from "./MenuEnlaces"
 import "./header.css"
 import "../../assets/logoportal.png"
+import Login from "./Login"
 
 const Encabezado = () => {// No nombrar funciones con etiquetas reservadas de HTML como header, body, etc.
     return (
         <div className="header">
-            <div class="menu">
-                <img src="../../assets/logoportal.png"></img>
-                <nav>
-                    <MenuEnlaces />
-                </nav>
-                <div class="empresas">
-                    <span class="material-icons vaca">
-                        login
-                    </span>
-                    <h6 class="vaca">EMPRESAS</h6>
-                </div>
-            </div>
+            <div className="quicksand">PORTAL DE EVENTOS</div>
+                <MenuEnlaces />
+            {/* <div class="empresas">
+                <span class="material-icons vaca">
+                    login
+                </span>
+                <h6 class="vaca">EMPRESAS</h6>
+            </div> */}
+            <Login />
         </div>
     )
 }
