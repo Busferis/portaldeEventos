5cca908fed8b67bea90e81f110ff21a00f281291
Busferis <busferis7@gmail.com>
2023-09-06 23:14:07 -0300

    Actualizacion

---
+++ src/components/body/Portal.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Portal = () => {
+    return(
+        <div className="cartel_padre">
+            <div className="cartel">
+                <h2>TE CONTAMOS QUIENES SOMOS</h2>
+                <h1>EL PORTAL</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum provident eveniet veniam, 
+                    facilis sed cumque ipsam facere a quam natus in similique ducimus aspernatur iste doloribus 
+                    pariatur dolore? Reiciendis, obcaecati.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Portal;
