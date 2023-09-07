06a8db583694af3712fb5d89543123b1d7f597c8
Busferis <busferis7@gmail.com>
2023-09-07 01:27:26 -0300

    Actualizacion

--- src/components/body/Secciones.jsx
+++ src/components/body/Secciones.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const Secciones = () => {
     return(
         <div className="secciones">
-            <div className="seccion">
+            <div className="seccion sec_img1">
                 <div className="barra_seccion">Ideas & Notas</div>
             </div>
-            <div className="seccion">
+            <div className="seccion sec_img2">
                 <div className="barra_seccion">Promociones</div>
             </div>
-            <div className="seccion">
+            <div className="seccion sec_img3">
                 <div className="barra_seccion">Eventos</div>
             </div>
         </div>
--- src/components/body/Slider.jsx
+++ src/components/body/Slider.jsx
@@ -3,12 +3,12 @@ import imagen from '../imgs/images.jpg'
 
 const Slider = () => {
     return(
-        <div className="portada2">
+        <div className="portada2 portada_img">
             <div className="cubito1">
-            <i className='material-icons-outlined'>arrow_back_ios</i>
+                <i className='material-icons-outlined'>arrow_back_ios</i>
             </div>
             <div className="cubito2">
-            <i className='material-icons-outlined'>arrow_forward_ios</i>
+                <i className='material-icons-outlined'>arrow_forward_ios</i>
             </div>
         </div>
     )
